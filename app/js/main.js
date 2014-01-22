1c287a863bd16264169a93aad61c67239015f203
sryoji <sryoji5087@gmail.com>
2014-01-22 23:07:05 +0900

    コンテンツ切替処理追加

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-	var nav = new Nav($("#main_menu"));
-	nav.setAction();
-
-	$(".selectpicker").selectpicker();
-
+	controller.set_action();
 });
+
+var controller = function() {
+	
+	var set_view_change_handler = function() {
+		$("body").on("click", "a.change-view", function(e){
+			e.preventDefault();
+			get_content($(this).attr("data-to"));
+		});
+	}
+
+	var get_content = function(to) {
+		$.get("./" + to + ".html", function(data){console.log(data)});
+	}
+	
+	return {
+		set_action : function() {
+			set_view_change_handler();
+		}
+	}
+}();
