c351b6a75a5fab5e2707615c0a3644a51762905f
sryoji <sryoji5087@gmail.com>
2014-01-23 15:34:59 +0900

    update map.js
    未完

--- app/js/map.js
+++ app/js/map.js
@@ -2,6 +2,30 @@ var map_controller = function() {
 
 	var map;
 
+	// 地図の中心を移動する
+	var chageCenter = function(_point) {
+		map.panTo(new google.maps.LatLng(39, 39));
+	}
+
+	// 地図上にマーカーを置く
+	var setMarker = function(_point) {
+		var opts = {
+			position: new google.maps.LatLng(),
+			map: map,
+			title: ""
+		}
+		var marker = new google.maps.Marker();
+		google.maps.event.addListener(marker, 'click', function(){
+			// 吹きだしを表示
+		});
+	}
+
+	var moveTo = function(_to) {
+		// 地図を移動
+
+		// 吹きだしを表示
+	}
+
 	return {
 		init: function() {
 			var latlng = new google.maps.LatLng(39,138);
