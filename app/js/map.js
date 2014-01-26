babc66fbd8e98dd69c3ab19c627c9560ed18ef2e
sryoji <sryoji5087@gmail.com>
2014-01-27 01:06:37 +0900

    マーカー処理を追加

--- app/js/map.js
+++ app/js/map.js
@@ -1,30 +1,71 @@
 var map_controller = function() {
 
 	var map;
+	var geo;
+	var data;
+	var marker_list = null;
 
 	// 地図の中心を移動する
 	var chageCenter = function(_point) {
 		map.panTo(new google.maps.LatLng(39, 39));
 	}
 
+	var setMarkers = function(_points) {
+		if (marker_list == null) {
+			marker_list = new google.maps.MVCArray();			
+		}else {
+			clearMarker();
+		}
+
+		for (var i = 0; i < _points.length; i++) {
+			setMarker(_points[i]);
+		};
+	}
+
 	// 地図上にマーカーを置く
 	var setMarker = function(_point) {
-		var opts = {
-			position: new google.maps.LatLng(),
-			map: map,
-			title: ""
-		}
-		var marker = new google.maps.Marker();
-		google.maps.event.addListener(marker, 'click', function(){
-			// 吹きだしを表示
+		var req = {"address": _point.address};
+		geo.geocode(req, function(result, status){
+			var opts = {
+				position: result[0].geometry.location,
+				map: map,
+				title: "<strong>"+ _point.name + "</strong><p>" + _point.text + "</p>"
+			}
+			var marker = new google.maps.Marker(opts);
+			marker_list.push(marker);
+			google.maps.event.addListener(marker, 'click', function(){
+				// 移動
+				map.panTo(marker.getPosition());
+				
+				// 吹き出し表示
+				var infowin = new google.maps.InfoWindow({
+					position: marker.getPosition(),
+					content: marker.getTitle()
+				});
+				infowin.open(map);
+			});
 		});
 	}
 
-	var moveTo = function(_to) {
-		// 地図を移動
-
-		// 吹きだしを表示
+	var clearMarker = function() {
+		marker_list.forEach(function(marker, index){
+			marker.setMap(null);
+		});
 	}
+
+	var setCurrentPosition = function() {
+
+		navigator.geolocation.getCurrentPosition(
+			function(posi) {
+				map.panTo(new google.maps.LatLng(posi.coords.latitude, posi.coords.longitude));
+				map.setZoom(15);
+			},
+			function(error) {
+				console.log(error);
+			}
+		);
+	}
+
 
 	return {
 		init: function() {
@@ -36,10 +77,22 @@ var map_controller = function() {
 			};
 
 			map = new google.maps.Map($("#map").get(0), opts);
+			geo = new google.maps.Geocoder();
+			setCurrentPosition();
+
+			$.getJSON('./data/map_data.json', function(json) {
+				data = json;
+			});
+
+			$("body").on('click', '.dropdown-menu li a', function(event) {
+				event.preventDefault();
+				/* Act on the event */
+				setMarkers(data[$(this).attr('href')]);
+			});
+		},
+		
+		test: function() {
+			clearMarker();
 		}
 	}
 }();
-
-$(document).ready(function() {
-	map_controller.init();
-});
